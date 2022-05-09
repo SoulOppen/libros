5e79201452a417d814907bc09f9c91a007d7dddb
oppen <arieloppenheimer@outlook.cl>
2022-05-09 09:39:37 -0400

    fin???

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
     enum status:[:Estante , :Prestado]
     ransacker :status, formatter: proc {|v| statuses[v]} do |parent|
         parent.table[:status]
